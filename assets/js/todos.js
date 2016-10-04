f146cdfb967add0a14b5ecc7c04b55dba42f034c
StephanRaab <sarcartist@gmail.com>
2016-10-03 23:40:23 -0400

    removing todos
    making sure to use remove() so that the actual todo gets deleted

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+//strike through todos
 $("li").on("click", function() {
 	$(this).toggleClass("strikethrough");
-})
+});
+
+//click on x to delete the todo
+$("span").on("click", function(event) {
+	$(this).parent().fadeOut(250, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
