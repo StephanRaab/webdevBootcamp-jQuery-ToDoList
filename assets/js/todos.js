7345cdeaeb92b499de60dee0ce6ce0bcc1e67319
StephanRaab <sarcartist@gmail.com>
2016-10-09 11:53:20 -0400

    Section 18 part 7
    - Get value from input
    - Clear input field
    - Append new li to ul

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
 //strike through todos
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("strikethrough");
 });
 
 //click on x to delete the todo
-$("span").on("click", function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(250, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//clear input field
+		$(this).val("");
+
+		//create new li and append to the ul
+		$('ul').append('<li><span>X</span> ' + todoText + '</li>');
+	}
+})
